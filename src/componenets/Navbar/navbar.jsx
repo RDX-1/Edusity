ad9e3381e130a3cf19c4b7ca42e39f237ac37196
Zain Ul Abideen <127882169+RDX-1@users.noreply.github.com>
2024-06-22 23:54:38 +0500

    Navbar

---
+++ src/componenets/Navbar/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./navbar.css"
+import logo from "../../assets/logo.png"
+
+const navbar = () => {
+  return (
+    <nav className='container'>
+      <img src={logo} alt="" className='logo'/>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Program</a></li>
+        <li><a href="#">Campus</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><button className="btn">Contact Us</button></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default navbar
