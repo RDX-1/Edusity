4f736a3f40e3f5688b6f38f5e40c021e9b08e5f8
Zain Ul Abideen <127882169+RDX-1@users.noreply.github.com>
2024-06-22 23:59:10 +0500

    href updated

--- src/componenets/Navbar/navbar.jsx
+++ src/componenets/Navbar/navbar.jsx
@@ -7,11 +7,11 @@ const navbar = () => {
     <nav className='container'>
       <img src={logo} alt="" className='logo'/>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Program</a></li>
-        <li><a href="#">Campus</a></li>
-        <li><a href="#">Testimonials</a></li>
-        <li><a href="#">About Us</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Program</a></li>
+        <li><a href="/">Campus</a></li>
+        <li><a href="/">Testimonials</a></li>
+        <li><a href="/">About Us</a></li>
         <li><button className="btn">Contact Us</button></li>
       </ul>
     </nav>
